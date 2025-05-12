a8bdd639a3938289e959ac495d2bb215252d2ad7
Mateus Felipe da Silveira VIeira <65318468+habdig7oficial@users.noreply.github.com>
2025-05-12 13:50:01 +0000

    done

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 compile:
 	gcc src/ex1.c -o ex1.elf
-	gcc src/ex2-3.c -o ex2-3.elf
+	gcc src/ex2-3-4.c -o ex2-3-4.elf
 
 run:
 	./ex1.elf
-	./ex2-3.elf
+	./ex2-3-4.elf 
 
 all: compile run
--- src/ex2-3-4.c
+++ src/ex2-3-4.c
@@ -1,7 +1,16 @@
 #include "stdio.h"
 #include "stdlib.h"
+#include "string.h"
 
 int main(int argc, char *argv[]){
+
+    char name_copy[30];
+
+    if(argv[1] == NULL)
+        strcpy(name_copy, "[NULL_NAME].txt");
+    else
+        strcpy(name_copy, argv[1]);
+
 
     FILE *file_ptr = fopen("saida.txt", "r");
 
@@ -14,17 +23,21 @@ int main(int argc, char *argv[]){
 
     char *buffer = malloc(f_size * sizeof(char));
 
-    char *teste = fgets(buffer, f_size, file_ptr);
+    FILE *copy_ptr = fopen(name_copy, "w");
+
+    char *line = fgets(buffer, f_size, file_ptr);
     int lines = 0;
-    while(teste != NULL){
+    while(line != NULL){
         printf("%s\n", buffer);
-        teste = fgets(buffer, f_size, file_ptr);
+        fprintf(copy_ptr, "%s", buffer); // copy to another file
+        line = fgets(buffer, f_size, file_ptr); // read each line of file
         lines++;
     }
 
     printf("This file has: %d lines\n", lines);
 
     fclose(file_ptr);
+    fclose(copy_ptr);
 
     return 0;
 }
